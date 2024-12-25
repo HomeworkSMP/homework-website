4edf0b5f63bd5182304c9b4e1754abf67b6b97fb
AvogatoWizardWhisker <babis_abdul@hotmail.fr>
2024-12-25 13:14:49 +0100

    Implemented TailwindCSS and testing its features

---
+++ HomeworkWebsite/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './**/*.razor',
+        './wwwroot/index.html',
+        './wwwroot/**/*.css',
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
